8cfeffa4c803a82ee2cc7de45e21ae20206395e6
tredmon <trevan.redmon@gmail.com>
2015-01-29 15:57:55 -0700

    footer auto-size

--- scripts/jqueryscript.js
+++ scripts/jqueryscript.js
@@ -1,13 +1,19 @@
 $(document).ready(function(){
-    $("footer").css("position", "absolute");
+    $("footer").css("position", "fixed");
     $("footer").css("bottom", "0px");
     document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
     var el = document.getElementsByTagName("body")[0].lastElementChild;
+    el.setAttribute("id", "footerspaceholder");
     el.setAttribute("class", "footer");
-    $("footer").css("width", ($("body").width() - parseInt($("footer").css("padding")))+"px");
-    $(el).css("height", $("footer").height());
-    $(el).css("width", $("footer").width());
     $(el).css("visibility", "hidden");
+    var footersizechg = function(){
+        $el = $("#footerspaceholder");
+        $("footer").css("width", ($("body").width() - parseInt($("footer").css("padding")))+"px");
+        $el.css("height", $("footer").height());
+        $el.css("width", $("footer").width());
+    };
+    footersizechg();
+    $(window).on("resize", footersizechg);
     
 });
 
